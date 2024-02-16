5b80408aea845229ebc81e80302b0da362f67673
ItzPanther <74084868+ItzPanther@users.noreply.github.com>
2024-02-16 20:45:31 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+  // Popup Open Function
+  function showPopup(popupId) {
+    document.getElementById(popupId).style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  // Popup Close Function
+  function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
